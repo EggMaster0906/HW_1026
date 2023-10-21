227c21c26785897abac98528d0136d65007368c9
EggMaster0906 <lsinpen@gmail.com>
2023-10-21 14:07:21 +0800

    HW3.18_1

---
+++ HW3.18/source/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	float sales;
+	while (1)
+	{
+		printf("Enter sales in dolars (-1 to end): ");
+		scanf("%f", &sales);
+
+		if (sales == -1)
+		{
+			break;
+		}
+		else
+		{
+			printf("Salary is: %.2f", sales*0.09 + 200);
+			printf("\n\n");
+		}
+	}
+	
+	return 0;
+}
